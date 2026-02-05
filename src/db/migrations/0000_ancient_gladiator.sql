afa26b9870d559a25b64e6a1540db16963270e8a
Bogdan Jovanovic <bogdan.jovanovic517@gmail.com>
2026-02-05 15:08:46 +0100

    organizacija baze i migracija

---
+++ src/db/migrations/0000_ancient_gladiator.sql
@@ -0,0 +1,52 @@
+CREATE TABLE "kalendar" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"datum" date NOT NULL,
+	"opis" varchar(255),
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "korisnik" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"ime" varchar(255) NOT NULL,
+	"slug" varchar(255) NOT NULL,
+	"email" varchar(255) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"role" varchar(50) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "korisnik_slug_unique" UNIQUE("slug"),
+	CONSTRAINT "korisnik_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "predmet" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"naziv" varchar(255) NOT NULL,
+	"slug" varchar(255) NOT NULL,
+	"opis" varchar(1000),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "predmet_slug_unique" UNIQUE("slug")
+);
+--> statement-breakpoint
+CREATE TABLE "prisustvo" (
+	"korisnik_id" uuid NOT NULL,
+	"raspored_id" uuid NOT NULL,
+	"datum_prisustva" date NOT NULL,
+	"status" varchar(50) NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "prisustvo_korisnik_id_raspored_id_datum_prisustva_pk" PRIMARY KEY("korisnik_id","raspored_id","datum_prisustva")
+);
+--> statement-breakpoint
+CREATE TABLE "raspored" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"dan_u_nedelji" varchar(20) NOT NULL,
+	"vreme_pocetka" time NOT NULL,
+	"vreme_zavrsetka" time NOT NULL,
+	"nastavni_dan" varchar(50),
+	"predmet_id" uuid NOT NULL,
+	"kalendar_id" uuid,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "prisustvo" ADD CONSTRAINT "prisustvo_korisnik_id_korisnik_id_fk" FOREIGN KEY ("korisnik_id") REFERENCES "public"."korisnik"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "prisustvo" ADD CONSTRAINT "prisustvo_raspored_id_raspored_id_fk" FOREIGN KEY ("raspored_id") REFERENCES "public"."raspored"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "raspored" ADD CONSTRAINT "raspored_predmet_id_predmet_id_fk" FOREIGN KEY ("predmet_id") REFERENCES "public"."predmet"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "raspored" ADD CONSTRAINT "raspored_kalendar_id_kalendar_id_fk" FOREIGN KEY ("kalendar_id") REFERENCES "public"."kalendar"("id") ON DELETE no action ON UPDATE no action;
